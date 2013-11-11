b60a3c7b5fc73ce104e9929ab2c1ea25c2862f09
glebtv <glebtv@gmail.com>
2013-11-11 16:06:34 +0400

    allow disabling indexing of models per instance

--- lib/mongoid/elasticsearch/indexing.rb
+++ lib/mongoid/elasticsearch/indexing.rb
@@ -6,9 +6,13 @@ module Mongoid
         def as_indexed_json
           serializable_hash.reject { |k, v| %w(_id c_at u_at created_at updated_at).include?(k) }
         end
+        
+        def es_index?
+          true
+        end
 
         def es_update
-          if destroyed?
+          if destroyed? || !es_index?
             self.class.es.remove_item(self)
           else
             self.class.es.index_item(self)
