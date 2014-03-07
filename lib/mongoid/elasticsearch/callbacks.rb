76304f5b08396c1f084b20bb54a534a3faca06a5
Sergey Malykh <xronos.i.am@gmail.com>
2014-03-07 14:35:34 +0400

    New method "Model.without_es_update!" to temporally disable callbacks (ie, for testing)

--- lib/mongoid/elasticsearch/callbacks.rb
+++ lib/mongoid/elasticsearch/callbacks.rb
@@ -4,13 +4,30 @@ module Mongoid
       extend ActiveSupport::Concern
 
       included do
-        after_save do
-          es_update
-        end
+        include InstanceMethods
 
-        after_destroy do
+        after_save :update_es_index
+        after_destroy :update_es_index
+      end
+
+      module InstanceMethods
+        def update_es_index
           es_update
         end
+      end
+
+      module ClassMethods
+        def without_es_update!( &block )
+          skip_callback( :save, :after, :update_es_index )
+          skip_callback( :destroy, :after, :update_es_index )
+          
+          result = yield
+
+          set_callback( :save, :after, :update_es_index )
+          set_callback( :destroy, :after, :update_es_index )
+          
+          result
+        end        
       end
     end
   end
